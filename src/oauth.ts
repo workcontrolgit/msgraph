365d6249003079ebb4bf4440e361e7c9435c421f
fuji.nguyen <fuji.nguyen@live.com>
2019-06-30 01:03:06 -0400

    init check in

---
+++ src/oauth.ts
@@ -0,0 +1,9 @@
+export const OAuthSettings = {
+  appId: '51fbd6bb-0c68-4716-b172-8ecbee28324e',
+  scopes: [
+    "user.read",
+    "calendars.read"
+  ]
+
+  //704d4abe-5c91-463d-bcc5-91799baeb436
+};
