866409b704259a22600c6003606f92b9343622aa
Fuji Nguyen <fuji.nguyen@live.com>
2019-07-01 08:24:07 -0400

    Update oauth.ts

--- src/oauth.ts
+++ src/oauth.ts
@@ -1,5 +1,5 @@
 export const OAuthSettings = {
-  appId: '51fbd6bb-0c68-4716-b172-8ecbee28324e',
+  appId: '704d4abe-5c91-463d-bcc5-91799baeb436',
   scopes: [
     "user.read",
     "calendars.read"
