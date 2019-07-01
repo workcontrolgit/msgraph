0f5262197f6a35389be9d033f6c97098fad68efa
Fuji Nguyen <fuji.nguyen@live.com>
2019-07-01 08:17:16 -0400

    Update oauth.ts

--- src/oauth.ts
+++ src/oauth.ts
@@ -5,5 +5,5 @@ export const OAuthSettings = {
     "calendars.read"
   ]
 
-  //704d4abe-5c91-463d-bcc5-91799baeb436
+  //com 704d4abe-5c91-463d-bcc5-91799baeb436 us 51fbd6bb-0c68-4716-b172-8ecbee28324e
 };
